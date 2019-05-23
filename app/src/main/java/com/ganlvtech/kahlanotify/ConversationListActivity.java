e20b8ba424eebc5a2d94f5eec20fc4f7d79e3bbc
EdgeNeko <EdgeNeko@aiursoft.com>
2019-05-23 21:31:09 +0800

    feat: add menu button in conversation list activity

--- app/src/main/java/com/ganlvtech/kahlanotify/ConversationListActivity.java
+++ app/src/main/java/com/ganlvtech/kahlanotify/ConversationListActivity.java
@@ -7,7 +7,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -21,15 +24,16 @@ import com.ganlvtech.kahlanotify.kahla.models.ContactInfo;
 import com.ganlvtech.kahlanotify.kahla.responses.auth.MeResponse;
 import com.ganlvtech.kahlanotify.kahla.responses.friendship.MyFriendsResponse;
 import com.ganlvtech.kahlanotify.util.ConversationListActivitySharedPreferences;
-import com.jaeger.library.StatusBarUtil;
 
 import java.util.List;
 
 public class ConversationListActivity extends MyServiceActivity {
     private DrawerLayout mDrawerLayoutConversationListActivity;
+    private ActionBarDrawerToggle mDrawerToggle;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private ListView mListViewConversations;
     private ListView mListViewAccounts;
+    private Toolbar mToolbar;
     private TextView mToolbalTextViewTitle;
     private TextView mToolbalTextViewSubtitle;
     private TextView mTextViewNewAccount;
@@ -50,8 +54,15 @@ public class ConversationListActivity extends MyServiceActivity {
         mListViewConversations = findViewById(R.id.listViewConversations);
         mListViewAccounts = findViewById(R.id.listViewAccounts);
         mTextViewNewAccount = findViewById(R.id.textViewNewAccount);
+        mToolbar = findViewById(R.id.toolbar);
 
-        StatusBarUtil.setColorNoTranslucentForDrawerLayout(this, mDrawerLayoutConversationListActivity, getColor(R.color.main_theme));
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayoutConversationListActivity,R.string.Menu,R.string.Menu);
+        mDrawerLayoutConversationListActivity.addDrawerListener(mDrawerToggle);
 
         mSwipeRefreshLayout.setColorSchemeColors(getColor(R.color.main_theme));
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
@@ -230,5 +241,13 @@ public class ConversationListActivity extends MyServiceActivity {
                 })
                 .setNegativeButton("Cancel", null)
                 .show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
